36f3bc8c5546ad875d3aaba2f35837098d7809b1
Luis Ogando <logando1193@gmail.com>
2023-03-10 13:58:40 -0400

    Added query to fetch prices by params

--- src/main/java/com/capitole/logandotest/api/repository/PriceRepository.java
+++ src/main/java/com/capitole/logandotest/api/repository/PriceRepository.java
@@ -2,11 +2,25 @@ package com.capitole.logandotest.api.repository;
 
 import com.capitole.logandotest.api.model.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface PriceRepository extends JpaRepository<Price, Long> {
     List<Price> findAll();
+
+    @Query(value = "SELECT P " +
+            "FROM Price P " +
+            "WHERE P.productId = :productId " +
+            "AND P.brandId = :brandId " +
+            "AND :requestDate BETWEEN P.startDate AND P.endDate " +
+            "ORDER BY P.priority DESC ")
+    List<Price> findPriceBy(
+            Long productId,
+            Long brandId,
+            LocalDateTime requestDate
+    );
 }
